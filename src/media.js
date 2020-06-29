9b8ebe7c9227e54ac47d104e175679c6e9452d20
Sree Sai Raghava <raghava.rym@gmail.com>
2020-06-29 15:53:39 +0530

    modified index.scss

---
+++ src/media.js
@@ -0,0 +1,9 @@
+// This is to add media break points
+
+export const breakPoints = {
+  mobileMin: "480px",
+  mobileMax: "959px",
+  tabletMin: "960px",
+  tabletMax: "1199px",
+  desktopMin: "1200px",
+};
